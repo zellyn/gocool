e533921ed32036f9d767b0922ff4ef54ba35fe5b
Zellyn Hunter <zellyn@gmail.com>
2014-11-19 10:05:46 -0800

    Updated testtypes.sh to run all tests.

--- testtypes.sh
+++ testtypes.sh
@@ -1,5 +1,3 @@
-set -e
-
 tempfoo=`basename $0`
 rm -rf $TMPDIR/$tempfoo.*
 OUTDIR=`mktemp -d -t ${tempfoo}`
@@ -10,21 +8,56 @@ go build cmd/typecheck/typecheck.go
 GOOD_FILES='simplearith basicclassestree expressionblock objectdispatchabort initwithself compare comparisons cycleinmethods letnoinit forwardinherits letinit newselftype basic overridingmethod letshadows neg methodcallsitself overriderenamearg isvoid overridingmethod3 inheritsObject scopes letselftype if methodnameclash trickyatdispatch stringtest overridingmethod2 simplecase assignment subtypemethodreturn dispatch io staticdispatch classes hairyscary.cl cells.cl list.cl'
 BAD_FILES='nomain badredefineint inheritsselftype returntypenoexist letself badmethodcallsitself badarith outofscope selftypeparameterposition missingclass selftypebadreturn attroverride selftyperedeclared badequalitytest2 badequalitytest dupformals self-assignment selfinformalparameter badwhilecond assignnoconform caseidenticalbranch inheritsbool inheritsstring badwhilebody anattributenamedself attrbadinit redefinedclass redefinedobject signaturechange trickyatdispatch2 letbadinit badargs1 overridingmethod4 badstaticdispatch baddispatch lubtest'
 
+CORRECT=0
+INCORRECT=0
+BADNAME=''
+BADWANT=''
+BADGOT=''
+
+succeed ()
+{
+  CORRECT=$((CORRECT+1))
+  echo " ok"
+  return 0
+}
+
+fail ()
+{
+  INCORRECT=$((INCORRECT+1))
+  echo " FAIL"
+  if [[ $BADNAME == '' ]]
+  then
+      BADNAME=$1
+      BADWANT=$2
+      BADGOT=$3
+  fi
+  return 1
+}
+
 echo "---- GOOD INPUT ----"
 for i in $GOOD_FILES
 do
-    echo "### $i"
-    ./typecheck testdata/typecheck/$i.test > $OUTDIR/$i.test.out || true
-    cmp -s testdata/typecheck/$i.test.out $OUTDIR/$i.test.out || diff -u testdata/typecheck/$i.test.out $OUTDIR/$i.test.out
+    echo -n "$i..."
+    ./typecheck testdata/typecheck/$i.test > $OUTDIR/$i.test.out
+    cmp -s testdata/typecheck/$i.test.out $OUTDIR/$i.test.out && succeed || fail $i testdata/typecheck/$i.test.out $OUTDIR/$i.test.out
 done
 echo "---- BAD INPUT ----"
 for i in $BAD_FILES
 do
-    echo "### $i"
+    echo -n "$i..."
     ./typecheck testdata/typecheck/$i.test > $OUTDIR/$i.test.out || true
-    cmp -s testdata/typecheck/$i.test.out $OUTDIR/$i.test.out || diff -u testdata/typecheck/$i.test.out $OUTDIR/$i.test.out
+    cmp -s testdata/typecheck/$i.test.out $OUTDIR/$i.test.out && succeed || fail $i testdata/typecheck/$i.test.out $OUTDIR/$i.test.out
 done
 
-echo 'SUCCESS!'
+if [[ $INCORRECT == 0 ]]
+then
+    echo "SUCCESS! ($CORRECT/$((CORRECT+INCORRECT)))"
+else
+    
+    echo -e "\nFirst failure: $BADNAME"
+    diff -u $BADWANT $BADGOT
+    echo "FAIL! ($CORRECT/$((CORRECT+INCORRECT)))"
+    exit 1
+fi
 rm -r $OUTDIR
 rm -f ./typecheck
