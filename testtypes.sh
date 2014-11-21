8aad054a20227358b3667d8e8fcf6122cb521c48
Zellyn Hunter <zellyn@gmail.com>
2014-11-20 22:59:46 -0800

    testtypes: fail hard on previously passing tests

--- testtypes.sh
+++ testtypes.sh
@@ -7,6 +7,8 @@ go build cmd/typecheck/typecheck.go || exit 1
 
 GOOD_FILES='simplearith basicclassestree expressionblock objectdispatchabort initwithself compare comparisons cycleinmethods letnoinit forwardinherits letinit newselftype basic overridingmethod letshadows neg methodcallsitself overriderenamearg isvoid overridingmethod3 inheritsObject scopes letselftype if methodnameclash trickyatdispatch stringtest overridingmethod2 simplecase assignment subtypemethodreturn dispatch io staticdispatch classes hairyscary.cl cells.cl list.cl'
 BAD_FILES='nomain badredefineint inheritsselftype returntypenoexist letself badmethodcallsitself badarith outofscope selftypeparameterposition missingclass selftypebadreturn attroverride selftyperedeclared badequalitytest2 badequalitytest dupformals self-assignment selfinformalparameter badwhilecond assignnoconform caseidenticalbranch inheritsbool inheritsstring badwhilebody anattributenamedself attrbadinit redefinedclass redefinedobject signaturechange trickyatdispatch2 letbadinit badargs1 overridingmethod4 badstaticdispatch baddispatch lubtest'
+# Previously passing tests: if we get a fail on one of these, we diff and exit immediately.
+PASSING='badredefineint inheritsbool inheritsselftype inheritsstring nomain redefinedclass redefinedobject selftyperedeclared'
 
 CORRECT=0
 INCORRECT=0
@@ -28,10 +30,17 @@ fail ()
   if [[ $BADNAME == '' ]]
   then
       BADNAME=$1
-      BADWANT=$2
-      BADGOT=$3
+      BADWANT=testdata/typecheck/$1.test.out 
+      BADGOT=$OUTDIR/$1.test.out
   fi
   return 1
+}
+
+unexpectedfail ()
+{
+    echo " FAIL (previously passing test)"
+    diff -u testdata/typecheck/$1.test.out $OUTDIR/$1.test.out
+    exit 1
 }
 
 echo "---- BAD INPUT ----"
@@ -39,7 +48,13 @@ for i in $BAD_FILES
 do
     echo -n "$i..."
     ./typecheck testdata/typecheck/$i.test > $OUTDIR/$i.test.out 2>&1
-    cmp -s testdata/typecheck/$i.test.out $OUTDIR/$i.test.out && succeed || fail $i testdata/typecheck/$i.test.out $OUTDIR/$i.test.out
+    if cmp -s testdata/typecheck/$i.test.out $OUTDIR/$i.test.out
+    then
+	succeed
+    else
+	[[ " $PASSING " == *" $i "* ]] && unexpectedfail $i
+	fail $i
+    fi
 done
 
 echo "---- GOOD INPUT ----"
@@ -47,17 +62,25 @@ for i in $GOOD_FILES
 do
     echo -n "$i..."
     ./typecheck testdata/typecheck/$i.test > $OUTDIR/$i.test.out 2>&1
-    cmp -s testdata/typecheck/$i.test.out $OUTDIR/$i.test.out && succeed || fail $i testdata/typecheck/$i.test.out $OUTDIR/$i.test.out
+    if cmp -s testdata/typecheck/$i.test.out $OUTDIR/$i.test.out
+    then
+	succeed
+    else
+	[[ " $PASSING " == *" $i "* ]] && unexpectedfail $i
+	fail $i
+    fi
 done
 
 if [[ $INCORRECT == 0 ]]
 then
-    echo "SUCCESS! ($CORRECT/$((CORRECT+INCORRECT)))"
+    echo "($CORRECT/$((CORRECT+INCORRECT)))"
+    echo "ok"
 else
     
     echo -e "\nFirst failure: $BADNAME"
     diff -u $BADWANT $BADGOT
-    echo "FAIL! ($CORRECT/$((CORRECT+INCORRECT)))"
+    echo "($CORRECT/$((CORRECT+INCORRECT)))"
+    echo "FAIL"
     exit 1
 fi
 rm -r $OUTDIR
