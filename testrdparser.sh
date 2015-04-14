70e126f6bd68d452934ee9cf2a3fec3a7686ef42
Zellyn Hunter <zellyn@gmail.com>
2015-04-13 17:36:55 -0700

    Don't care about error text for r-d fails.

--- testrdparser.sh
+++ testrdparser.sh
@@ -8,7 +8,7 @@ OUTDIR=`mktemp -d -t ${tempfoo}`
 go build cmd/printparse/printparse.go
 
 GOOD_FILES='classonefield classtwofields assignment dispatchnoargs comparisons-assoc staticdispatchnoargs dispatchonearg nestedblocks addedlet letnoinit classmethodonearg letinit assignseq unaryassociativity dispatcharglist whileexpressionblock ifexpressionblock letassociativity whileoneexpression letinitmultiplebindings letparens multipleattributes multipledispatches associativity+ associativity- associativity-times associativitydiv arithprecedence ifnested multipleatdispatches casemultiplebranch formallists nestedlet complex atoi'
-BAD_FILES='emptyprogram classnoname emptymethodbody attrcapitalname baddispatch4 emptystaticdispatch baddispatch3 equalsassociativity baddispatch2 returntypebad classbadinherits lteassociativity whilenoloop extrasemicolonblock ifnothenbranch classbadname badblock badfeatures ifnoelsebranch ifnoelse emptyassign firstbindingerrored secondbindingerrored badfeaturenames ifnofi firstclasserrored baddispatch1 badexprlist newbadtype while assigngetstype isvoidbadtype multiplemethoderrors multipleclasses casenoexpr'
+BAD_FILES='emptyprogram classnoname emptymethodbody attrcapitalname baddispatch4 emptystaticdispatch baddispatch3 equalsassociativity baddispatch2 returntypebad classbadinherits lteassociativity whilenoloop extrasemicolonblock ifnothenbranch classbadname badblock ifnoelsebranch emptyassign firstbindingerrored secondbindingerrored firstclasserrored baddispatch1 badexprlist newbadtype while assigngetstype isvoidbadtype multiplemethoderrors multipleclasses casenoexpr badfeaturenames ifnofi badfeatures ifnoelse'
 
 echo "---- GOOD INPUT ----"
 for i in $GOOD_FILES
@@ -21,8 +21,14 @@ echo "---- BAD INPUT ----"
 for i in $BAD_FILES
 do
     echo "### $i"
-    ./printparse -rd testdata/parse/$i.test > $OUTDIR/$i.test.out || true
-    cmp -s testdata/parse/$i.test.out $OUTDIR/$i.test.out || diff -u testdata/parse/$i.test.out $OUTDIR/$i.test.out
+    ./printparse -rd testdata/parse/$i.test > $OUTDIR/$i.test.out && rc=$? || rc=$?
+	if [ "$rc" -eq "0" ]
+	then
+		echo "FAIL: unexpected successful parse!"
+		exit
+	fi
+	# Don't care about exact error messages
+    # cmp -s testdata/parse/$i.test.out $OUTDIR/$i.test.out || diff -u testdata/parse/$i.test.out $OUTDIR/$i.test.out
 done
 
 echo 'SUCCESS!'
